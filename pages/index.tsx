f7a034297140998f0ced5b0a3b138b1ae7bb5ede
Razak <moabdulrazak17@gmail.com>
2022-09-16 15:41:18 +0000

    chore: folder structure and dummy components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from 'next';
+import { Header } from './components/Header';
+import { List } from './components/List';
+import { Map } from './components/Map';
+import { PlaceDetails } from './components/PlaceDetails';
 
 const Home: NextPage = () => {
-	return <div>Hello Mom!</div>;
+	return (
+		<div>
+			<Header />
+			<List />
+			<Map />
+			<PlaceDetails />
+		</div>
+	);
 };
 
 export default Home;
