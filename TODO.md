125878b6c2179f1a11368e84818d3947b919cb1f
Razak <moabdulrazak17@gmail.com>
2022-09-16 17:49:38 +0000

    feat(components): google map react template

--- TODO.md
+++ TODO.md
@@ -1,12 +1,12 @@
 ### Dependencies
 
 - Bootstrap next js (with typescript)
-yarn create next-app travel-buddy --ts
+  yarn create next-app travel-buddy --ts
 
 - Install Dependencies
 
 - Material UI Stuff
-yarn add @mui/lab @mui/material @mui/icons-material @emotion/react @emotion/styled @fontsource/roboto @emotion/cache @emotion/server tss-react
+  yarn add @mui/lab @mui/material @mui/icons-material @emotion/react @emotion/styled @fontsource/roboto @emotion/cache @emotion/server tss-react
 
 - Other Dependencies
-yarn add @react-google-maps/api axios google-map-react react-icons
+  yarn add @react-google-maps/api axios google-map-react react-icons
--- components/Map.tsx
+++ components/Map.tsx
@@ -1,3 +1,58 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, Rating, useMediaQuery } from '@mui/material';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+
+import useStyles from '../styles/Map';
+
+interface Place {
+	name: string;
+	numReviews: number;
+	rating: number;
+	priceLevel: number;
+	phone: string;
+	address: string;
+	webUrl: string;
+	website: string;
+	awards: object;
+	cuisine: object;
+	photos: object;
+	location: {
+		lat: number;
+		lng: number;
+	};
+}
+
+type WeatherData = {
+	temperature: number;
+	description: string;
+	icon: string;
+	wind: number;
+	humidity: number;
+};
+
+interface MapProps {
+	coordinates: number[];
+	weatherData: WeatherData;
+	places: Place[];
+	setCoordinates: React.Dispatch<React.SetStateAction<number[]>>;
+	setBounds: React.Dispatch<React.SetStateAction<number[][]>>;
+	setChildClicked: React.Dispatch<React.SetStateAction<Place | null>>;
+}
+
 export const Map = () => {
-	return <div>Map</div>;
+	const { classes } = useStyles();
+
+	return (
+		<div className={classes.mapContainer}>
+			<GoogleMapReact
+				bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+				defaultZoom={14}
+				margin={[50, 50, 50, 50]}
+				options={''}
+				onChange={''}
+				onChildClick={''}
+			></GoogleMapReact>
+		</div>
+	);
 };
