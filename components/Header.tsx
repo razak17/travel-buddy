f30aafdb2ed3f8c7cd3d60dca38e07717e8facfc
Razak <moabdulrazak17@gmail.com>
2022-09-16 17:05:14 +0000

    feat(components): create navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,30 @@
+import { AppBar, Toolbar, Box, Typography, InputBase } from '@mui/material';
+// import { Autocomplete } from '@react-google-maps/api';
+import SearchIcon from '@mui/icons-material/Search';
+
+import useStyles from '../styles/Header';
+
 export const Header = () => {
+	const { classes } = useStyles();
+
 	return (
-    <h1>Navbar</h1>
+		<AppBar position='static'>
+			<Toolbar className={classes.toolbar}>
+				<Typography variant='h5' className={classes.title}>
+					Travel Advisor
+				</Typography>
+				<Box display='flex'>
+					<Typography variant='h6' className={classes.title}>
+						Explore new places
+					</Typography>
+					<div className={classes.search}>
+						<div className={classes.searchIcon}>
+							<SearchIcon />
+						</div>
+						<InputBase placeholder='Search…' classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+					</div>
+				</Box>
+			</Toolbar>
+		</AppBar>
 	);
 };
